11537a49861dd90bd37bce716810ee301a83e116
Rhythm Patel <prhythm3099@gmail.com>
2023-10-02 10:26:03 -0700

    chore: add diffing logic for deviceRoles and deviceSchema

--- generateDiff.go
+++ generateDiff.go
@@ -55,9 +55,19 @@ func doGenerateDiff(cmd *SubCommand, client *cb.DevClient, args ...string) error
 		return err;
 	}
 
+	logInfo("Diffing devices and deviceRoles:");
+	diffDevices, diffDeviceRoles, diffDeviceSchema, err := getDevicesDiff(systemInfo.Key, client);
+
+	if err != nil {
+		return err;
+	}
+
 	dataMap := make(map[string]interface{});
 	dataMap["services"] = diffServices;
 	dataMap["libraries"] = diffLibraries;
+	dataMap["devices"] = diffDevices;
+	dataMap["devicesRoles"] = diffDeviceRoles;
+	dataMap["deviceSchema"] = diffDeviceSchema;
 
 	err = storeDataInJSONFile(dataMap, PathForDiffFile, "diff.json");
 
@@ -97,8 +107,6 @@ func getLibrariesDiff(systemKey string, client *cb.DevClient) ([]string, error) 
 		}
 	}
 
-	fmt.Println(librariesDiff)
-
 	fmt.Printf("\n")
 	return librariesDiff, nil
 }
@@ -131,7 +139,81 @@ func getServicesDiff(systemKey string, client *cb.DevClient) ([]string, error) {
 		}
 	}
 
-	fmt.Println(servicesDiff)
 	fmt.Printf("\n")
 	return servicesDiff, nil
+}
+
+func getDevicesDiff(systemKey string, client *cb.DevClient) ([]string, []string, bool, error) {
+	// does diffing for devices, deviceRoles as well as deviceSchema
+
+	devicesDiff := []string{}
+	deviceRolesDiff := []string{}
+
+	localDevices, err := getDevices()
+	
+	if err != nil {
+		return nil, nil, false, err
+	}
+
+	for _, localDevice := range localDevices {
+		localDeviceName := (localDevice["name"]).(string)
+
+		fmt.Printf(localDeviceName + " ");
+
+		remoteDevice, err := pullDevice(systemKey, localDeviceName, client)
+
+		if err != nil {
+			// remoteDevice not found in the system, but is present locally. This should be added in our diff
+			devicesDiff = append(devicesDiff, localDeviceName)
+			deviceRolesDiff = append(deviceRolesDiff, localDeviceName)
+			continue;
+		}
+
+		localDevice, remoteDevice = keepCommonKeysFromMaps(localDevice, remoteDevice)
+
+		if !reflect.DeepEqual(localDevice, remoteDevice) {
+			devicesDiff = append(devicesDiff, localDeviceName);
+		}
+
+		localDeviceRole, err := getDeviceRoles(localDeviceName)
+		if err != nil {
+			return nil, nil, false, err
+		}
+
+		remoteDeviceRole, err := pullDeviceRoles(systemKey, localDeviceName, client)
+		if err != nil {
+			return nil, nil, false, err
+		}
+
+		if !reflect.DeepEqual(convertInterfaceSliceToStringSlice(localDeviceRole), remoteDeviceRole) {
+			deviceRolesDiff = append(deviceRolesDiff, localDeviceName)
+		}
+	}
+
+	localDeviceSchema, err := getDevicesSchema()
+	if err != nil {
+		return nil, nil, false, err;
+	}
+
+	remoteDeviceSchema, err := pullDevicesSchema(systemKey, client, false)
+	if err != nil {
+		return nil, nil, false, err;
+	}
+
+	if !areLocalAndRemoteDeviceSchemaEqual(localDeviceSchema, remoteDeviceSchema) {
+		return devicesDiff, deviceRolesDiff, true, nil;
+	} else {
+		return devicesDiff, deviceRolesDiff, false, err
+	}
+}
+
+func areLocalAndRemoteDeviceSchemaEqual(localDeviceSchema map[string]interface{}, remoteDeviceSchema map[string]interface{}) bool {
+	// we need this util function because localDeviceSchema has entry like map[columns:<nil>] to show that there are no columns
+  // whereas remoteDeviceSchema has entry like map[columns:[]] to show that there are no columns
+
+	if localDeviceSchema["columns"] == nil && len(remoteDeviceSchema["columns"].([]interface{})) == 0 {
+		return true;
+	} else {
+		return reflect.DeepEqual(localDeviceSchema["columns"], remoteDeviceSchema["columns"])
+	}
 }
