c4f8dd0219d314f6d4480e3b23707d0701a42dab
Matthew Walowski <mattwalowski@gmail.com>
2024-03-12 08:48:36 -0500

    Remove toStringArray from utils

--- models/systemUpload/dryRun/utils.go
+++ models/systemUpload/dryRun/utils.go
@@ -5,17 +5,6 @@ import (
 	"strings"
 )
 
-func toStringArray(val interface{}) []string {
-	result := make([]string, 0)
-
-	arr := val.([]interface{})
-	for _, item := range arr {
-		result = append(result, item.(string))
-	}
-
-	return result
-}
-
 func writeDryRunSection(sb *strings.Builder, title, contents string) {
 	sb.WriteString(fmt.Sprintf("-- %s --\n", title))
 	sb.WriteString(contents)
