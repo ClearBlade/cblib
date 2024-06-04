1e5978a5521c72b28feb4accc4aae261c32b5760
Clark Bynum <cbynum@clearblade.com>
2024-06-04 15:14:06 -0500

    fix push schema not pushing new columns

--- colutil/colutil.go
+++ colutil/colutil.go
@@ -7,7 +7,13 @@ import (
 
 func GetDiffForColumnsWithDynamicListOfDefaultColumns(localSchemaInterfaces, backendSchemaInterfaces []map[string]interface{}) *diff.UnsafeDiff[map[string]interface{}] {
 	return listutil.CompareListsAndFilter[map[string]interface{}](localSchemaInterfaces, backendSchemaInterfaces, columnExists, func(a map[string]interface{}) bool {
-		return a["UserDefined"].(bool)
+		// if the UserDefined key exists, that means the column exists on the backend.
+		userDefined, ok := a["UserDefined"].(bool)
+		if ok {
+			return userDefined
+		}
+		// if the UserDefined key does not exist, that means the column does not exist yet on the backend and should be added.
+		return true
 	})
 }
 
