f086a24298bfc3607e9acb1bd33c0d9f2c6f5e03
sky-sharma <sky.sharma@gmail.com>
2026-03-11 14:12:08 -0700

    Mod writeService for case where user ID could not be resolved to email for run_user because user wasn't pulled before OR user is developer

--- fs.go
+++ fs.go
@@ -827,10 +827,12 @@ func writeService(name string, data map[string]interface{}) error {
 	// Store run_user as email so that pushing to a different system resolves
 	// to the correct user ID in the target system via the platform's ValidateCodeMeta.
 	if runUser, ok := data["run_user"].(string); ok && runUser != "" {
-		if email, err := getUserEmailByID(runUser); err == nil {
+		if email, err := getUserEmailByID(runUser); err != nil {
+			fmt.Printf("Warning - Could not resolve run_user %q to an email for service %q: %s\n", runUser, name, err)
+		} else if email != runUser {
 			data["run_user"] = email
 		} else {
-			fmt.Printf("Warning - Could not resolve run_user %q to an email: %s\n", runUser, err)
+			fmt.Printf("Warning - Could not resolve run_user %q to an email for service %q. Either the user was not pulled previously OR the run_user is set to a developer account.\n", runUser, name)
 		}
 	}
 
